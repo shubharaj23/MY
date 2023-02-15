167e86979fb642c47572744f7334a6d34f50bf91
shubharaj23 <124787828+shubharaj23@users.noreply.github.com>
2023-02-15 21:11:42 +0530

    Add files via upload

---
+++ my_captain2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 20:29:32 2023
+
+@author: dell
+"""
+
+list1=[12,-7,5,64,-14]
+list1.pop()
+list1.pop(1)
+print(list1)
+list2=[12,14,-95,3]
+list2.pop(2)
+print(list2)
